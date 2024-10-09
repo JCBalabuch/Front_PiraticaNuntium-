84ff9e802e60b35de0500b760626ae5327975e86
Jeniffer Balabuch <balabuchj@gmail.com>
2024-10-08 22:44:10 -0400

    START Front App Piratica Nuntium

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './main.scss';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Pirática Nuntium</h1>
+    <img src='./public/assets/PiraticaNuntiumWOBG.png'/>
+  </div>
+`;
---
+++ src/utils/fetch.js
@@ -0,0 +1,6 @@
+export const news = [];
+
+export const fetchNews = () => {
+  const dbURL =
+    'mongodb+srv://balabuchj:evVBUl7fqsWh8eeJ@cluster0.nydtu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+};
